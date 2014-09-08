c7f1b7bb7be07f0b841f981b65d8c18c80a927fb
Corbin Page <corbin.page@gmail.com>
2014-09-08 19:06:26 -0400

    Leaving Daily Grind

--- popup.js
+++ popup.js
@@ -1,14 +1,25 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-// Called when the user clicks on the browser action.
+
+// 1) This script executes
+// 2) The popup is rendered
+alert("Start Script");
+chrome.tabs.executeScript({
+  code: 'alert(window.getSelection().toString())'
+});
+
+var node = document.createElement('h2');
+// alert(window.getSelection().toString());
+node.innerHTML = 'Hello';
+// alert(node);
+document.getElementById("body").appendChild(node);
+// alert(document);
+
+
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
+  alert('hello ' + document.location.href);
   console.log('Turning ' + tab.url + ' red!');
   chrome.tabs.executeScript({
     code: 'document.body.style.backgroundColor="red"'
   });
 });
-
-alert('hello ' + document.location.href);
