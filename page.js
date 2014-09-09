9ba8705036f690f3d85b5b021976f2a4f9db2e23
Corbin Page <corbin.page@gmail.com>
2014-09-09 00:31:27 -0400

    Added bootstrap

--- page.js
+++ page.js
@@ -1,1 +1,17 @@
-page.js
+function getSelectionText() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log("Typo Blaster message received...");
+    var text = getSelectionText();
+    sendResponse({text: text});
+  }
+);
--- popup.js
+++ popup.js
@@ -2,24 +2,18 @@
 
 // 1) This script executes
 // 2) The popup is rendered
-alert("Start Script");
-chrome.tabs.executeScript({
-  code: 'alert(window.getSelection().toString())'
-});
-
-var node = document.createElement('h2');
-// alert(window.getSelection().toString());
-node.innerHTML = 'Hello';
-// alert(node);
-document.getElementById("body").appendChild(node);
-// alert(document);
 
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  alert('hello ' + document.location.href);
-  console.log('Turning ' + tab.url + ' red!');
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  
+  console.log("Sending Message...");
+
+  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+    console.log(response.text);
+
+    var node = document.createElement('h2');
+    node.innerHTML = response.text;
+    document.getElementById("body").appendChild(node);
+
   });
 });
