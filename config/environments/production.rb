336164a5a47eb618b205e2d3343074230fdc59cc
Corbin Page <corbin.page@gmail.com>
2014-09-13 17:27:43 -0400

    Changed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Google Analytics
-  GA.tracker = "UA-44648226-4" 
+  GA.tracker = "UA-44648226-4"  
 
   # Code is not reloaded between requests.
   config.cache_classes = true
