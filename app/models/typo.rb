95eb7ceee4784f766c95b1f1aa2f9bdf2a655c78
Corbin Page <corbin.page@gmail.com>
2014-09-22 15:44:07 -0400

    Troubleshooting

--- app/models/typo.rb
+++ app/models/typo.rb
@@ -5,15 +5,16 @@ class Typo < ActiveRecord::Base
   STATUS_EMAIL_SENT = "EMAIL_SENT"
 
   def self.save_new(params)
-    new_typo = Typo.new
+    new_typo = Typo.new(params[:typo]);
 
     puts "Params: " + params.inspect
 
-    new_typo[:user_email] = params[:user_email]
-    new_typo[:highlighted_text] = params[:highlighted_text]
-    new_typo[:complete_text] = params[:complete_text]
-    new_typo[:comments] = params[:comments]
-    new_typo[:url] = params[:url]
+    # new_typo[:status]
+    # new_typo[:user_email] = params[:user_email]
+    # new_typo[:highlighted_text] = params[:highlighted_text]
+    # new_typo[:complete_text] = params[:complete_text]
+    # new_typo[:comments] = params[:comments]
+    # new_typo[:url] = params[:url]
     new_typo[:status] = STATUS_READY_FOR_EMAIL
     new_typo[:domain_id] = Domain.find_or_create_by(url: params[:domain_text]).id
 
