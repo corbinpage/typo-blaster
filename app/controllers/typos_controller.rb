75c126fb66d64701e188fe97e79fb7da66f77525
Corbin Page <corbin.page@gmail.com>
2014-09-22 15:04:48 -0400

    Getting all parameters of the POST working

--- app/controllers/typos_controller.rb
+++ app/controllers/typos_controller.rb
@@ -2,7 +2,7 @@ class TyposController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    puts typo_params.inspect
+    puts "After: " + typo_params.inspect
     @typo = Typo.save_new(params)
   end
 
@@ -11,7 +11,8 @@ class TyposController < ApplicationController
 
   private
   def typo_params
-    params.permit(:user_email, :highlighted_text, :complete_text, :comments, :url)
+    puts "Before: " + params.inspect
+    params.require(:typo).permit(:user_email, :highlighted_text, :complete_text, :comments, :url, :domain_text)
     # params.require(:typo).permit(:user_email, :highlighted_text, :complete_text, :comments, :url)
   end
 
