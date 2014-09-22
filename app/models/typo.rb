69726566f39027715dd42166025b92c4b0110aef
Corbin Page <corbin.page@gmail.com>
2014-09-22 16:42:37 -0400

    Post requests are now working

--- app/models/typo.rb
+++ app/models/typo.rb
@@ -5,22 +5,10 @@ class Typo < ActiveRecord::Base
   STATUS_EMAIL_SENT = "EMAIL_SENT"
 
   def self.save_new(typo_params)
-    puts "Params1: " + typo_params.inspect
     domain_text = typo_params.delete("domain_text")
-    puts "Params2: " + typo_params.inspect
     new_typo = Typo.new(typo_params)
-    puts "Typo3: " + new_typo.inspect
-
-    # new_typo[:status]
-    # new_typo[:user_email] = params[:user_email]
-    # new_typo[:highlighted_text] = params[:highlighted_text]
-    # new_typo[:complete_text] = params[:complete_text]
-    # new_typo[:comments] = params[:comments]
-    # new_typo[:url] = params[:url]
     new_typo[:status] = STATUS_READY_FOR_EMAIL
     new_typo[:domain_id] = Domain.find_or_create_by(url: domain_text).id
-
-    puts "New Typo: " + new_typo.inspect
 
     new_typo.save
     new_typo
